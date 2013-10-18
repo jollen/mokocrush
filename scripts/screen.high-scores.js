3338bf895ee12a00c591f627040b5cf62c63a94d
Hank Wang <drapho@gmail.com>
2013-10-18 23:39:52 +0800

    Use local scores server

--- scripts/screen.high-scores.js
+++ scripts/screen.high-scores.js
@@ -1,40 +1,27 @@
 jewel.screens["high-scores"] = (function() {
     var json = {};
-    var ws = new WebSocket("ws://direct.mokoversity.com:8888/", "game-protocol");
-    ws.onmessage = wsOnMessage;
+
+    var ws = new WebSocket("ws://"+location.hostname+':8888', "game-protocol");
+    // var ws = new WebSocket("ws://direct.mokoversity.com:8888/", "game-protocol");
+    ws.onmessage = function(message) {
+        json = JSON.parse(message.data);
+        $('#scoresList').html($.tmpl($("#scoresTemplate").html(), json));
+    };
     ws.onopen = function() {
         console.log("WebSocket rocks");
     };
     ws.onclose = function() {
         console.log("Close");
-        console.log(ws);
     };
     ws.onerror = function(e) {
         console.log('error');
     };
 
-    function wsOnMessage(message) {
-        json = JSON.parse(message.data);
-        showScoreboard();
+    function sendScores(scoreData) {
+        console.log('Send score to server through WebSocket');
+        ws.send(JSON.stringify(scoreData));
     }
 
-    function sendScores(score) {
-        ws.send(JSON.stringify(score));
-    }
-
-    function showScoreboard() {
-        json.sort(function(a, b) {
-            if (a.scores < b.scores) {
-                return 1;
-            } else if (a.scores > b.scores) {
-                return -1;
-            } else {
-                return 0;
-            }
-        });
-        $('#scoresList').html($.tmpl($("#scoresTemplate").html(), json));
-
-    }
     function run() {
 
     }
